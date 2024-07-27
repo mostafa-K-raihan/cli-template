01387d3cb38cca3c355339a2d4d4e3918b4c7f8c
raihan <mostafa.raihan@newscred.com>
2024-07-27 15:41:44 +0600

    Add ci

--- a.sh
+++ a.sh
@@ -40,3 +40,45 @@ outDir: "dist"
 .gitignore >>dist
 pnpm add -D @types/node (console, will not work)
 pnpm add commander
+pnpm add -D tsup
+# noEmit: true,
+add lint: "tsc"
+add build: "tsup src/index.ts --format cjs,esm --dts"
+pnpm add -D vitest
+add test: "vitest run"
+modify dev: "vitest"
+add test file index.test.ts
+# import { describe, expect, it } from "vitest";
+#
+# describe("cli", () => {
+#   it("dummy test", () => {
+#     expect(1).toBe(1);
+#   });
+# });
+#
+#
+add ci: pnpm lint && pnpm test && pnpm build
+add .github/workflows/main.yml
+# name: CI
+# on:
+#   push:
+#     branches:
+#       - "**"
+#
+# jobs:
+#   build:
+#     runs-on: ubuntu-latest
+#     steps:
+#       - uses: actions/checkout@v4
+#       - uses: pnpm/action-setup@v4
+#         with:
+#           version: 8
+#       - uses: actions/setup-node@v4
+#         with:
+#           node-version: 20
+#           cache: "pnpm"
+#
+#       - run: pnpm i --frozen-lockfile
+#       - run: pnpm run ci
+#
+
